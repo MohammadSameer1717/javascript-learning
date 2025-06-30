c0156edf9b42fd09ed5c9d32da71a1dc114241c8
Mohammad Sameer <mohammadsameer.171717@gmail.com>
2025-07-01 00:31:17 +0530

    jsAsync

---
+++ JSAsync.js
@@ -0,0 +1,110 @@
+let user  = {
+    id: 2,
+    name: "pop",
+    email: "pop@gmail.com",
+    isActive: false,
+};
+
+setTimeout(() => {
+    user.isActive = true;
+    console.log("test inside");
+
+    if (user.isActive) {
+    console.log(user.name);
+    
+}
+}, 3000);
+
+console.log("test outside");
+
+if (user.isActive) {
+    console.log(user.name);
+}
+
+
+
+// promises
+let user = {
+    id: 2,
+    name: "pop",
+    email: "pop@gmail.com",
+    isActive: false,
+};
+
+const wait = (num) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            user.isActive = true;
+            resolve();
+        }, num);
+    });
+};
+
+// This is the correct structure
+async function main() {
+    await wait(3000); 
+    if (user.isActive) {
+        console.log(user.name); 
+    }
+}
+main();
+// this is a correct structures( callback hell swichtion )
+const fetchUser = () => {
+    return {
+        id: 2,
+        name: "push",
+    };
+};
+const  userProducts = (userId) => {
+
+}
+const fetchUserDetails = () => {
+    fetchUser((userId) => {
+        const products = userProducts(userId, (productsId) =>{
+            const category = productscategory(productsId)
+        });
+    });
+};
+
+let user  = {
+    id: 2,
+    name: "pop",
+    email: "pop@gmail.com",
+    isActive: false,
+};
+
+const wait = (num) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            user.isActive = true;
+            resolve();
+        }, num);
+    });
+};
+const checkUser = async () => {
+    console.log("first");
+
+    await wait(3000);
+    if (user.isActive) {
+    }
+    console.log("second");
+};
+checkUser();
+
+
+
+// one of the best topic in js
+let posts = [];
+ fetch ('https://jsonplaceholder.typicode.com/posts')
+ .then((res) => res.json())
+ .then((data) => {
+    posts = [...data];
+    console.log(posts);
+});
+ 
+const fetchposts = async () => {
+    const response = await  fetch ('https://jsonplaceholder.typicode.com/posts');
+    posts = await response.json();
+    console.log(posts);
+};
+fetchposts();
--- arrayMethods.js
+++ arrayMethods.js
@@ -1,256 +1,258 @@
-var arr1 = [45, 56, 43, 20, 51, 66];
-var sum = 0;
+// var arr1 = [45, 56, 43, 20, 51, 66];
+// var sum = 0;
 
-arr1.forEach(function(element, Index) {
-    console.log(Index, element);
-    sum += element;
-});
-console.log(sum);
+const { use } = require("react");
 
-var arr1 = [35, 56, 47, 20, 51, 55];
-arr1.forEach(function(element, Index) {
-    console.log(Index, element);
-});
+// arr1.forEach(function(element, Index) {
+//     console.log(Index, element);
+//     sum += element;
+// });
+// console.log(sum);
+
+// var arr1 = [35, 56, 47, 20, 51, 55];
+// arr1.forEach(function(element, Index) {
+//     console.log(Index, element);
+// });
 
 
 
- var users = [
-    {
-    name: "peter",
-    email: "peter@gmail.com",
-    email2: "sam@gmail.com",
-    isActive: true,
-    createdAt: new Date(),
-    products:["iphone", "airpods", "macbook"],  
-    age: 56
-    },
+//  var users = [
+//     {
+//     name: "peter",
+//     email: "peter@gmail.com",
+//     email2: "sam@gmail.com",
+//     isActive: true,
+//     createdAt: new Date(),
+//     products:["iphone", "airpods", "macbook"],  
+//     age: 56
+//     },
 
-    {
-    name: "salman",
-    email: "salman@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["tresemme", "beardo gel", "phillips led"], 
-    age: 70  
-    },
+//     {
+//     name: "salman",
+//     email: "salman@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["tresemme", "beardo gel", "phillips led"], 
+//     age: 70  
+//     },
 
-   {
-    name: "samarth",
-    email: "samarth@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 30
-    },
-      {
-    name: "terenaam",
-    email: "terenaam@gmail.com",
-    isActive: true,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 40
-    },
-      {
-    name: "goti",
-    email: "goti@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 60
-    },
- ];
+//    {
+//     name: "samarth",
+//     email: "samarth@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 30
+//     },
+//       {
+//     name: "terenaam",
+//     email: "terenaam@gmail.com",
+//     isActive: true,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 40
+//     },
+//       {
+//     name: "goti",
+//     email: "goti@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 60
+//     },
+//  ];
 
- users.forEach(function (item, Index) {
-    if (item, isActive) {
-        console.log(item);
-    }
- });
-users.forEach(function (item, Index) {
-    if (item.age > 50) {
-        item.products.forEach(function (product, Index) {
-            console.log(product);
-        });
-    }
-});
+//  users.forEach(function (item, Index) {
+//     if (item, isActive) {
+//         console.log(item);
+//     }
+//  });
+// users.forEach(function (item, Index) {
+//     if (item.age > 50) {
+//         item.products.forEach(function (product, Index) {
+//             console.log(product);
+//         });
+//     }
+// });
 
-users.forEach(function(item, Index) {
-    item.contact = Math.floor(Math.random() *100000000000);
-    console.log(item);
+// users.forEach(function(item, Index) {
+//     item.contact = Math.floor(Math.random() *100000000000);
+//     console.log(item);
     
-});
-// start new session ....
+// });
+// // start new session ....
  
-var arr1 = [34, 43, 54, 65, 76, 87, 98];
+// var arr1 = [34, 43, 54, 65, 76, 87, 98];
 
-console.log(arr1.length);
-arr1.push(54);
-console.log(arr1);
+// console.log(arr1.length);
+// arr1.push(54);
+// console.log(arr1);
 
-var removed = arr1.pop();
-console.log(removed);
+// var removed = arr1.pop();
+// console.log(removed);
 
-console.log(arr1);
+// console.log(arr1);
 
-arr1.unshift(45,56);
-console.log(arr1);
-arr1.shift();
-console.log(arr1);
+// arr1.unshift(45,56);
+// console.log(arr1);
+// arr1.shift();
+// console.log(arr1);
 
-var arr1 = [34, 43, 54, 65, 76, 87, 98];
-arr1.splice(2, 2);
-console.log(arr1);
+// var arr1 = [34, 43, 54, 65, 76, 87, 98];
+// arr1.splice(2, 2);
+// console.log(arr1);
 
 
-var numArr = [56,71,53,84,96]
-// [56, "even", 71, "odd", 53, "even", 84, "odd", 96, "even"];
-var newaRR = [];
-numArr.forEach(function(item, Index) {
-    if (item % 2 === 0) {
-        newaRR.push(item, "even");
-    } else {
-        newaRR.push(item, "odd");
-    }
-});
-console.log(newaRR);
+// var numArr = [56,71,53,84,96]
+// // [56, "even", 71, "odd", 53, "even", 84, "odd", 96, "even"];
+// var newaRR = [];
+// numArr.forEach(function(item, Index) {
+//     if (item % 2 === 0) {
+//         newaRR.push(item, "even");
+//     } else {
+//         newaRR.push(item, "odd");
+//     }
+// });
+// console.log(newaRR);
 
- var users = [
-    {
-    name: "peter",
-    email: "peter@gmail.com",
-    email2: "sam@gmail.com",
-    isActive: true,
-    createdAt: new Date(),
-    products:["iphone", "airpods", "macbook"],  
-    age: 56
-    },
+//  var users = [
+//     {
+//     name: "peter",
+//     email: "peter@gmail.com",
+//     email2: "sam@gmail.com",
+//     isActive: true,
+//     createdAt: new Date(),
+//     products:["iphone", "airpods", "macbook"],  
+//     age: 56
+//     },
 
-    {
-    name: "salman",
-    email: "salman@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["tresemme", "beardo gel", "phillips led"], 
-    age: 70  
-    },
+//     {
+//     name: "salman",
+//     email: "salman@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["tresemme", "beardo gel", "phillips led"], 
+//     age: 70  
+//     },
 
-   {
-    name: "samarth",
-    email: "samarth@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 30
-    },
-      {
-    name: "terenaam",
-    email: "terenaam@gmail.com",
-    isActive: true,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 40
-    },
-      {
-    name: "goti",
-    email: "goti@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 60
-    },
- ];
+//    {
+//     name: "samarth",
+//     email: "samarth@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 30
+//     },
+//       {
+//     name: "terenaam",
+//     email: "terenaam@gmail.com",
+//     isActive: true,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 40
+//     },
+//       {
+//     name: "goti",
+//     email: "goti@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 60
+//     },
+//  ];
 
- var newUsers = [];
- users.forEach(function(item, Index) {
-    if (item.age > 45) {
-        newUsers.push(item);
-    } else {
-        newUsers.unshift(item);
-    }
- });
- console.log(newUsers);
+//  var newUsers = [];
+//  users.forEach(function(item, Index) {
+//     if (item.age > 45) {
+//         newUsers.push(item);
+//     } else {
+//         newUsers.unshift(item);
+//     }
+//  });
+//  console.log(newUsers);
  
-var numArr = [56,71,53,84,96,77,55,99,20];
-// var newArr = numArr.slice(2);
-var newArr = numArr.slice(2,6);
-console.log(newArr);
+// var numArr = [56,71,53,84,96,77,55,99,20];
+// // var newArr = numArr.slice(2);
+// var newArr = numArr.slice(2,6);
+// console.log(newArr);
 
 
-var numArr = [56,71,53,84,96,77,55,99,20];
-var newArr = numArr.map (function(item, Index) {
-    return item * 2;
-});
+// var numArr = [56,71,53,84,96,77,55,99,20];
+// var newArr = numArr.map (function(item, Index) {
+//     return item * 2;
+// });
 
-console.log(newArr);
-var newArr = numArr.filter(function(item, Index) {
-    if (item % 2 !=0) {
-        return true;
-    } else {
-        return false;
-    }
-});
-console.log(newArr);
+// console.log(newArr);
+// var newArr = numArr.filter(function(item, Index) {
+//     if (item % 2 !=0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// });
+// console.log(newArr);
 
 
- var users = [
-    {
-    name: "peter",
-    email: "peter@gmail.com",
-    email2: "sam@gmail.com",
-    isActive: true,
-    createdAt: new Date(),
-    products:["iphone", "airpods", "macbook"],  
-    age: 56
-    },
+//  var users = [
+//     {
+//     name: "peter",
+//     email: "peter@gmail.com",
+//     email2: "sam@gmail.com",
+//     isActive: true,
+//     createdAt: new Date(),
+//     products:["iphone", "airpods", "macbook"],  
+//     age: 56
+//     },
 
-    {
-    name: "salman",
-    email: "salman@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["tresemme", "beardo gel", "phillips led"], 
-    age: 70  
-    },
+//     {
+//     name: "salman",
+//     email: "salman@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["tresemme", "beardo gel", "phillips led"], 
+//     age: 70  
+//     },
 
-   {
-    name: "samarth",
-    email: "samarth@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 30
-    },
-      {
-    name: "terenaam",
-    email: "terenaam@gmail.com",
-    isActive: true,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 40
-    },
-      {
-    name: "goti",
-    email: "goti@gmail.com",
-    isActive: false,
-    createdAt: new Date(), 
-    products:["washing machin", "vivo", "job"],  
-    age: 60
-    },
- ];
+//    {
+//     name: "samarth",
+//     email: "samarth@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 30
+//     },
+//       {
+//     name: "terenaam",
+//     email: "terenaam@gmail.com",
+//     isActive: true,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 40
+//     },
+//       {
+//     name: "goti",
+//     email: "goti@gmail.com",
+//     isActive: false,
+//     createdAt: new Date(), 
+//     products:["washing machin", "vivo", "job"],  
+//     age: 60
+//     },
+//  ];
 
- var element = users.find(function(item,Index) {
-    if (item.email === "samarth@gmail.com") {
-        return true;
-    }
- });
- console.log(element);
-console.log(element.name);
+//  var element = users.find(function(item,Index) {
+//     if (item.email === "samarth@gmail.com") {
+//         return true;
+//     }
+//  });
+//  console.log(element);
+// console.log(element.name);
 
- var elementIndex = users.findIndex(function(item,Index) {
-    if (item.email === "samarth@gmail.com") {
-        return true;
-    }
- });
- console.log(elementIndex);
+//  var elementIndex = users.findIndex(function(item,Index) {
+//     if (item.email === "samarth@gmail.com") {
+//         return true;
+//     }
+//  });
+//  console.log(elementIndex);
  
 
   var users = [
@@ -350,7 +352,7 @@ const user1 = {
 console.log(user1);
 console.log(user);
 
-REST OPERATOR and 
+// REST OPERATOR and 
 
 let user = {
   id: 191,
@@ -383,21 +385,25 @@ let user = {
   },
 };
 
-let { products, email, ...userObj } = user;
-console.log(products);
-console.log(email);
+ let { products, email, ...userObj } = user;
+ console.log(products);
+ console.log(email)
+ console.log(userObj);
 
+
+
+ const { className, year } = user.class;
+ console.log(className);
+
+ const { class: { className, year} } = user;
+ console.log(className);
+
+
+const { class: classObj, ... userObj} = user;
+console.log(classObj);
 console.log(userObj);
 
 
-
-const { className, year } = user.class;
-console.log(className);
-
-const { class: { className, year} } = user;
-console.log(className);
-
- 
 
 
 
